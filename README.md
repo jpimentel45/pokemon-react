09ec73b5233c2f90260a66a572f1b9183c03d34a
jpimentel45 <jpimentel45@gmail.com>
2019-09-01 10:14:44 -0700

    inital heroku push

--- README.md
+++ README.md
@@ -1,68 +1,32 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+// Deleted code need to fix 
+  const catchPokemon = (pokemon) => {
+        
+        wildEscapes() == 0 ?
+        
+        (
+        
+        //set state by passing through current state
+        // const min = 0
+        // const max = 3
+        // const willCatch = Math.floor(Math.random() * (max - min)) + min
+        willCatch() == 0 ?
+       ( (
+        setPokedex(state => {
+            //if current state already has id of pokemon
+            const monExists = (state.filter(p => pokemon.id == p.id).length > 0)
+            //if doesnt exist add pokemon to current state, and sort by id
+            if (!monExists) {
+                state = [...state, pokemon]
+                state.sort((a, b) => a.id - b.id)
 
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+            }
+            return state
+        })
+        
+            ) &&
+            encounterWildPokemon()) : alert("Pokemon Got Out of PokeBall")
+            //will automatically call after each true or fales from above ternary
+        //encounterWildPokemon()
+        ) 
+            : encounterWildPokemon() && alert('pokemon got away')  ;
+    }
---
+++ src/components/Encounter.js
@@ -0,0 +1,170 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+//import './index.css';
+
+function Encounter() {
+
+    const [pokedex, setPokedex] = useState([])
+    const [wildPokemon, setWildPokemon] = useState({});
+
+    //effect to encounte wild pokemon
+    //If you want to prevent from encounterWildPokemon from running 
+    //after deleting each pokemon remove pokedex from useEffect
+    //only thing useEffect wont run after successfully catching pokemon
+    //[pokedex] || [] chose one and set on useEffect
+    useEffect(() => { encounterWildPokemon() }, [pokedex])
+
+    //Get random pokemon id
+    const pokeId = () => {
+        //ciel:
+        const min = Math.ceil(1)
+        const max = Math.floor(807)
+        return Math.floor(Math.random() * (max - min)) + min
+    }
+
+    //create a ranFail have 
+    //alert cant run 
+    // attack? have pokemon attack : pokemon runs away
+    //encouter random
+    const encounterWildPokemon = () => {
+        {
+            alert('Oh shit! A Poochemon wants to fight!')
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId()}`)
+                .then(response => {
+                    console.log(response.data)
+                    setWildPokemon(response.data)
+                    { (alert(`Encountered ${response.data.name}, Let's BATALlll!!!`)) }
+                })
+
+        }
+    }
+    //Generate random number if not 0 it will escape
+    const wildEscapes = () => {
+        const min = 0
+        const max = 3
+        return Math.floor(Math.random() * (max - min)) + min
+    }
+
+    //function to dodge & escape catch if wildEscaped != 0
+    const escaped = () => {
+        alert(`${wildPokemon.name} Ran away from PokeBall`)
+        encounterWildPokemon()
+    }
+    //Pokemon escapes after failing to catch 
+    const fleeSuccess = () => {
+        alert(`${wildPokemon.name} Got Out of PokeBall and Ran away`)
+        encounterWildPokemon()
+    }
+
+    //Escape after pokeball?
+    const escapeBall = () => {
+        const min = 0
+        const max = 3
+        const fleeBall = Math.floor(Math.random() * (max - min)) + min
+        fleeBall === 0 ?
+            fleeSuccess()
+            : alert(`${wildPokemon.name} Got Out of PokeBall`)
+    }
+
+    //Run away successfully
+    const ranSuccess = () => {
+        alert("Ran away successfully")
+        encounterWildPokemon()
+    }
+    //if willRun = 0, Run away from Pokemon 
+    const run = () => {
+        const min = 0
+        const max = 2
+        const willRun = Math.floor(Math.random() * (max - min)) + min
+        willRun === 0 ?
+            ranSuccess() : alert(`You can't run from ${wildPokemon.name}`)
+    }
+
+
+    //Generate random number if not 0 won't catch
+    const willCatch = () => {
+        const min = 0
+        const max = 3
+        return Math.floor(Math.random() * (max - min)) + min
+    }
+
+
+    //func: Catch pokemon
+    const catchPokemon = (pokemon) => {
+        (willCatch() === 0 ?
+            ((
+                //set state by passing through current state
+                setPokedex(state => {
+                    //if current state already has id of pokemon
+                    const monExists = (state.filter(p => pokemon.id === p.id).length > 0)
+                    //if doesnt exist add pokemon to current state, and sort by id
+                    if (!monExists) {
+                        state = [...state, pokemon]
+                        state.sort((a, b) => a.id - b.id)
+                        alert(`Caught ${wildPokemon.name}`)
+                    }
+                    //if does exist encounter new
+                    //de
+                    else { encounterWildPokemon() }
+                    return state
+                })
+
+            )
+                &&
+                encounterWildPokemon())
+            :
+            //ternary inside willCatch Ternary
+            //is wildEscapes() === 0 poke will run encounter new poke
+            // or escapeBall, if ===0 poke should run away after pokeball encounter new
+            //or alert pokemon escaped pokeball
+            (wildEscapes() === 0 ? escaped() : escapeBall())//have ternary statement with gets outs of pokeball: escape or stay to fight
+            //will automatically call after each true or fales from above ternary
+            //encounterWildPokemon()
+        )
+        //(alert(`Encountered ${wildPokemon.name}, Let's BATALLLL!!!`))
+
+    }
+
+    //Release Poochimannnn
+    const releasePokemon = id => {
+   alert(`Releasing Pokemon into the wild`)
+        setPokedex(state => state.filter(p => p.id !== id))
+    }
+
+
+    return (
+        //render(
+        <div className="app-wrapper">
+            <header>
+                <h1 className='title'> React Hooks</h1>
+                <h3 className='subtitle'>With PoKemon</h3>
+            </header>
+            <section className='wild-pokemon'>
+                <h2>Wild Pokemon</h2>
+                <img src={`https://raw.githubusercontent.com/PokeApi/sprites/master/sprites/pokemon/${wildPokemon.id}.png`} className='sprite' />
+                <h3>{wildPokemon.name}</h3>
+                <button className='catch-btn' onClick={() => catchPokemon(wildPokemon)}>CATCH!!!!!!!</button>
+                <button className='catch-btn' onClick={() => run()}>RUN!!!</button>
+            </section>
+
+            <section className='pokedex'>
+                <h2>Pokedex</h2>
+                <div className='pokedex-list'>
+                    {pokedex.map(pokemon => (
+                        <div className="pokemon" key={pokemon.id}>
+                            <img src={`https://raw.githubusercontent.com/PokeApi/sprites/master/sprites/pokemon/${pokemon.id}.png`} className='sprite' />
+                            <h3 className='pokemon-name'>{pokemon.name}</h3>
+
+                            <button className="remove" onClick={() => releasePokemon(pokemon.id)}>&times;</button>
+                        </div>
+                    ))}
+                </div>
+            </section>
+
+        </div>
+        // )  
+    )
+
+}
+
+export default Encounter
